8b0fa8f810f2f40187bd8e5ff5b2a07dc5f08f06
GITUDORIS <gitudoris405@gmail.com>
2022-09-15 07:25:06 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Add two integers by function.
+ * @i: First integer variable to add.
+ * @j: Second variable to sum.
+ * Return: return with sum.
+ */
+int add(int i, int j)
+{
+	int n;
+	 n = i + j;
+	return (n);
+}
